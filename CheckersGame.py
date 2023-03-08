311d69a96c2fa246beb2d217ec291a628b42e667
lamn5 <noddylam24@gmail.com>
2023-03-08 00:00:01 -0500

    Add docstrings to class

--- CheckersGame.py
+++ CheckersGame.py
@@ -5,7 +5,9 @@
 # Description: 
 
 class Checkers:
-
+    """
+    Checkers class to represent the checkers game which contains two players. Player with black checkers always start first. Contains methods that return the number of captured pieces, name of winner, and details of a particular square. Uses the Player class to store information about the player and its pieces.
+    """
     def __init__(self):
         """
         Constructor for Checkers class. Takes no parameters. Initializes the board with the checkers in the correct positions. All data members are private.
@@ -43,7 +45,9 @@ class Checkers:
         pass
 
 class Player:
-
+    """
+    Player class to represent each player that is playing the game. Contains information such as the player's name and its piece color. Used by the Checkers class.
+    """
     def __init_(self):
         """
         Constructor for Player class. Takes no parameters. Initializes the player's name and piece color. All data members are private.

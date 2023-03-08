dabaf4bd0f1b33853c03f8be7e117f3a46c95c00
lamn5 <noddylam24@gmail.com>
2023-03-07 23:50:07 -0500

    Add proper class and methods

--- CheckersGame.py
+++ CheckersGame.py
@@ -3,3 +3,67 @@
 # GitHub username: lamn5
 # Date: 03/19/2023
 # Description: 
+
+class Checkers:
+
+    def __init__(self):
+        """
+        Constructor for Checkers class. Takes no parameters. Initializes the board with the checkers in the correct positions. All data members are private.
+        """
+        pass
+
+    def create_player(self, player_name, piece_color):
+        """
+        
+        """
+        pass
+
+    def play_game(self, player_name, starting_square_location, destination_square_location):
+        """
+        
+        """
+        pass
+
+    def get_checker_details(self, square_location):
+        """
+        
+        """
+        pass
+
+    def print_board(self):
+        """
+
+        """
+        pass
+
+    def game_winner(self):
+        """
+        
+        """
+        pass
+
+class Player:
+
+    def __init_(self):
+        """
+        Constructor for Player class. Takes no parameters. Initializes the player's name and piece color. All data members are private.
+        """
+        pass
+
+    def get_king_count(self):
+        """
+        
+        """
+        pass
+
+    def get_triple_king_count(self):
+        """
+        
+        """
+        pass
+
+    def captured_pieces_count(self):
+        """
+        
+        """
+        pass

7578641bd18f9734d2948302b5f89fec719a2c0f
lamn5 <noddylam24@gmail.com>
2023-03-20 02:02:13 -0400

    Add more unit tests

--- CheckersGameTester.py
+++ CheckersGameTester.py
@@ -3,7 +3,8 @@ from CheckersGame import Checkers, Player, InvalidSquareError
 
 class TestCheckersGame(unittest.TestCase):
 
-    def test_1(self):
+    def test_print_board_successfully(self):
+        """check the the first value of the print board method is None to make sure it's set up properly"""
         game = Checkers()
         self.assertIs(game.print_board(),None)
 
@@ -18,7 +19,16 @@ class TestCheckersGame(unittest.TestCase):
         with self.assertRaises(InvalidSquareError):
             game.get_checker_details((8,6))
 
+    def test_player_piece_moved_to_destination_successfully(self):
+        """tests that the checker piece has moved to its destination correctly"""
+        game = Checkers()
+        Player1 = game.create_player("Adam", "White")
+        game.play_game("Adam", (2,1),(3,0))
+        self.assertEqual(game.get_checker_details((3,0)),'White')
 
+    def test_game_winner(self):
+        """tests that the correct game winner is returned"""
+        
 
 if __name__ == '__main__':
     unittest.main()
